d78008ff7e8107bc50267fbf6e92691b62c72b0a
Jeff Haemer <jeffrey.haemer@gmail.com>
2026-04-05 14:30:15 -0600

    chore: restructure tree to move source under src/
    Name collisions mean I have to rename expenses_demo.py to expenses.py
    Ah well.

---
+++ src/expenses_demo/__init__.py
@@ -0,0 +1,15 @@
+from .expenses import (
+    Category as Category,
+    Expense as Expense,
+    ExpenseRepository as ExpenseRepository,
+    User as User,
+    demo_relationship as demo_relationship,
+)
+
+__all__ = [
+    "Category",
+    "Expense",
+    "ExpenseRepository",
+    "User",
+    "demo_relationship",
+]
